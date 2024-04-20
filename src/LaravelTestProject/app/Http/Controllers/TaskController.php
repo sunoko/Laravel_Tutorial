1452a4c17546eb74bf431faebde4c8c2cdc04fbe
KazukiChinone <staybesidehangon@gmail.com>
2024-04-21 01:06:40 +0900

    third

---
+++ src/LaravelTestProject/app/Http/Controllers/TaskController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    // indexメソッドを追加する
+    public function index()
+    {
+        return "Hello world";
+    }
+}
--- src/LaravelTestProject/routes/web.php
+++ src/LaravelTestProject/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+/* TaskControllerクラスを名前空間でインポートする */
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/* Laravel welcome Page */
 Route::get('/', function () {
     return view('welcome');
 });
+/* index page */
+Route::get("/folders/{id}/tasks", [TaskController::class,"index"])->name("tasks.index");
