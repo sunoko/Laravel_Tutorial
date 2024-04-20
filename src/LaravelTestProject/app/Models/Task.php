ee22df25d7f3ed7a170edbcb065b9e9c15a9732d
KazukiChinone <staybesidehangon@gmail.com>
2024-04-21 02:32:59 +0900

    ToDoアプリのタスク一覧表示機能を作る

---
+++ src/LaravelTestProject/app/Models/Task.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    /**
+     * ステータス（状態）定義
+     * const：定数
+     */
+    const STATUS = [
+        1 => [ 'label' => '未着手', 'class' => 'label-danger' ],
+        2 => [ 'label' => '着手中', 'class' => 'label-info' ],
+        3 => [ 'label' => '完了', 'class' => '' ],
+    ];
+
+    /**
+     * ステータス（状態）ラベルのアクセサメソッド
+     * 
+     * @return string
+     */
+    public function getStatusLabelAttribute()
+    {
+        // ステータス（状態）カラムの値を取得する
+        $status = $this->attributes['status'];
+
+        // STATUSに定義されていない場合
+        if (!isset(self::STATUS[$status])) {
+            // 空文字を返す
+            return '';
+        }
+        // STATUSの値（['label']）を返す
+        return self::STATUS[$status]['label'];
+    }
+
+    /**
+     * 状態を表すHTMLクラスのアクセサメソッド
+     * 
+     * @return string
+     */
+    public function getStatusClassAttribute()
+    {
+        $status = $this->attributes['status'];
+
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        return self::STATUS[$status]['class'];
+    }
+
+    /**
+     * 整形した期限日のアクセサメソッド
+     * 
+     * @return string
+     */
+    public function getFormattedDueDateAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d', $this->attributes['due_date'])
+            ->format('Y/m/d');
+    }
+}
