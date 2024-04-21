c94567c8a34ad6f1242a7c33cfbee6e62b5441d4
KazukiChinone <staybesidehangon@gmail.com>
2024-04-21 17:38:24 +0900

    ToDoアプリのフォルダ編集機能を作る

--- src/LaravelTestProject/app/Http/Controllers/FolderController.php
+++ src/LaravelTestProject/app/Http/Controllers/FolderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Folder;
 use App\Http\Requests\CreateFolder;
+use App\Http\Requests\EditFolder;
 
 class FolderController extends Controller
 {
@@ -37,6 +38,43 @@ class FolderController extends Controller
         // タイトルに入力値を代入する
         $folder->title = $request->title;
         // インスタンスの状態をデータベースに書き込む
+        $folder->save();
+
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
+    }
+
+    /**
+     *  【フォルダ編集ページの表示機能】
+     *
+     *  GET /folders/{id}/edit
+     *  @param int $id
+     *  @return \Illuminate\View\View
+     */
+    public function showEditForm(int $id)
+    {
+        $folder = Folder::find($id);
+
+        return view('folders/edit', [
+            'folder_id' => $folder->id,
+            'folder_title' => $folder->title,
+        ]);
+    }
+
+    /**
+     *  【フォルダの編集機能】
+     *
+     *  POST /folders/{id}/edit
+     *  @param int $id
+     *  @param EditTask $request
+     *  @return \Illuminate\Http\RedirectResponse
+     */
+    public function edit(int $id, EditFolder $request)
+    {
+        $folder = Folder::find($id);
+
+        $folder->title = $request->title;
         $folder->save();
 
         return redirect()->route('tasks.index', [
